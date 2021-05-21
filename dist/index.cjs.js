05dcb5d17f44c3a77b2e27885cf42a5eb66f741f
Ben Token <admin@bentoken.finance>
2021-05-20 22:42:06 -0700

    45

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2870,10 +2870,10 @@ var baseColors = {
 var brandColors = {
     binance: "#F0B90B",
 };
-var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#FAF9FA", backgroundDisabled: "#E9EAEB", contrast: "#261d13", invertedContrast: "#FFFFFF", input: "#f4efea", tertiary: "#EFF4F5", text: "#333333", textDisabled: "#BDC2C4", textSubtle: "#777777", borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
+var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#FAF9FA", backgroundDisabled: "#E9EAEB", contrast: "#261d13", invertedContrast: "#FFFFFF", input: "#f4efea", tertiary: "#f5f3ef", text: "#333333", textDisabled: "#BDC2C4", textSubtle: "#444444", borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
-var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#343135", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#261e13", input: "#5a4d3f", primaryDark: "#ffb948", tertiary: "#473e35", text: "#fff", textDisabled: "#666171", textSubtle: "#e0dcd6", borderColor: "#524B63", card: "#2c2926", gradients: {
+var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#353331", backgroundDisabled: "#423d37", contrast: "#FFFFFF", invertedContrast: "#261e13", input: "#5a4d3f", primaryDark: "#ffb948", tertiary: "#473e35", text: "#fff", textDisabled: "#716a61", textSubtle: "#e0dcd6", borderColor: "#63584b", card: "#2c2926", gradients: {
         bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
     } });
 
