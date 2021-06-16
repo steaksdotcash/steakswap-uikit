e3c30df78c5b59d1f429b73a478a75d57bf8b805
Cat <cat@coins.builders>
2021-06-16 00:02:40 -0700

    23

--- src/widgets/Menu/Logo.tsx
+++ src/widgets/Menu/Logo.tsx
@@ -23,7 +23,7 @@ const StyledLink = styled(Link)`
     }
   }
   .desktop-icon {
-    width: 156px;
+    width: 170px;
     display: none;
     ${({ theme }) => theme.mediaQueries.nav} {
       display: block;
--- src/widgets/Menu/icons/Logo.tsx
+++ src/widgets/Menu/icons/Logo.tsx
@@ -9,8 +9,8 @@ interface LogoProps extends SvgProps {
 const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
   const textColor = isDark ? "#FFFFFF" : "#000000";
   return (
-    <Svg viewBox="0 0 205 26" {...props}>
-      <image width="205" height="26" href={isDark ? '/images/ben/LogoTextNewDark.png' : '/images/ben/LogoTextNewWhite.png'}/>
+    <Svg viewBox="0 0 170 26" {...props}>
+      <image width="170" height="26" href={isDark ? '/images/ben/LogoTextNewDark.png' : '/images/ben/LogoTextNewWhite.png'}/>
     </Svg>
   )
 };
